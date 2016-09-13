f2c4f86142ad3915f79c3eca964334cb7669b25e
Artur Stachecki <artur.stachecki@gmail.com>
2016-09-13 16:30:12 +0200

    copy default config upon installation and align it with docker-compose setup

--- avlan/config/bin/avlan-install.sh
+++ avlan/config/bin/avlan-install.sh
@@ -6,6 +6,10 @@ set -e
 # TODO: figure out better way of handling remote repositories
 cp -r /repositories/avlan/* /repositories/avlan/.??* . && \
 
+# Install default settings
+# TODO: these should be imported from default dir
+cp /etc/default/avlan-config.py src/conf/settings.py
+
 PYTHON=/venv/bin/python
 PIP=/venv/bin/pip
 
--- avlan/config/etc/default/avlan-config.py
+++ avlan/config/etc/default/avlan-config.py
@@ -6,10 +6,10 @@ environ = os.environ
 admin_user = "admin",
 admin_password = "password"
 
-db_host = "localhost"
+db_host = "mysql.internal"
 db_port = 3306
-db_username = "root"
-db_password = "reverse"
+db_username = "avlan"
+db_password = "avlan"
 db_database = "avlan"
 
 translation_dir = "src/translations/"

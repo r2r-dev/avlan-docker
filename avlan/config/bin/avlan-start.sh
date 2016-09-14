03931dbfb52f28674ddc2202b0fa2f191fe28ce9
Artur Stachecki <artur.stachecki@gmail.com>
2016-09-14 17:32:52 +0200

    use proper sql dump name

--- avlan/config/bin/avlan-start.sh
+++ avlan/config/bin/avlan-start.sh
@@ -4,5 +4,5 @@
 rm -rf /app/webroot/* && cp -r /repositories/avlan/webroot/* /app/webroot/ 
 
 cd /app
-/venv/bin/python manage.py syncdb --dump scripts/database.sql
+/venv/bin/python manage.py syncdb --dump scripts/bootstrap.sql
 /venv/bin/gunicorn -b 0.0.0.0:8000 __init__:application

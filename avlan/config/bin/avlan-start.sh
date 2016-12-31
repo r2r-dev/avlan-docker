ddaec3cf6f8f338233e29ae3515f7467ebef39fb
Artur Stachecki <artur.stachecki@gmail.com>
2016-12-31 20:41:47 +0100

    increase number of gunicorn workers and timeout

--- avlan/config/bin/avlan-start.sh
+++ avlan/config/bin/avlan-start.sh
@@ -5,4 +5,4 @@ rm -rf /app/webroot/* && cp -r /resources/avlan/webroot/* /app/webroot/
 
 cd /app
 /venv/bin/python manage.py syncdb --dump resources/bootstrap.sql && \
-/venv/bin/gunicorn -b 0.0.0.0:8000 __init__:application
+/venv/bin/gunicorn -b 0.0.0.0:8000 --workers 3 --timeout 360 __init__:application
